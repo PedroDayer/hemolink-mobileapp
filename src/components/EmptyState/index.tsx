8d23470d4534626921da3232ff26c6475abddfec
Pedro Dayer <pedrob.dayer@gmail.com>
2026-06-18 17:08:20 -0300

    feat: criacao e estruturacao do componente 'EmptyState' + instalacao da dependencia do lottiefiles

--- src/components/EmptyState/index.tsx
+++ src/components/EmptyState/index.tsx
@@ -5,12 +5,34 @@
 //  -Nenhum favorito salvo;
 //  -Você não possui notificações.
 
-import { Text, View } from "react-native";
+// Nota: Exemplo de implementacao:
 
-export const EmptyState = () => {
+//  import { EmptyState } from "../../../components/EmptyState";
+//  <EmptyState message={"Nao há dados para esse filtro"} messageStyle={{fontSize:20}} />
+//  <EmptyStatesource={require(".../seiLa.json")} />
+import { StyleProp, Text, TextStyle, View, ViewStyle } from "react-native";
+import LottieView, { AnimationObject } from "lottie-react-native";
+
+interface PropsEmptyState {
+  message?: string;
+  messageStyle?: TextStyle;
+  containerStyle?: ViewStyle;
+  lottieStyle?: StyleProp<ViewStyle>;
+  source?: string | AnimationObject;
+}
+
+// OBS: O componente começa com estilos próprios de tamanho da animaçao + caminho da animacao no arquivo assets, caso não passem nada de props para ele
+export const EmptyState = ({
+  message,
+  containerStyle,
+  messageStyle,
+  source = require("../../assets/animacoes/EmptyBox.json"),
+  lottieStyle = { width: 200, height: 200 },
+}: PropsEmptyState) => {
   return (
-    <View>
-      <Text></Text>
+    <View style={[containerStyle]}>
+      <LottieView source={source} autoPlay loop style={lottieStyle} />
+      {message && <Text style={[messageStyle]}>{message}</Text>}
     </View>
   );
 };
