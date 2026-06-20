39d79e9750324bebc79b730b05c8fc9c8de66455
LuizFelipe <luizfelipe.vor@gmail.com>
2026-06-19 21:14:53 -0300

    criacao das rotas crud hospital

--- src/services/HospitalService.ts
+++ src/services/HospitalService.ts
@@ -28,12 +28,48 @@ export interface BloodStock {
   "O-": number;
 }
 
-export async function listarHospitais(): Promise<Hospital[]> {
-    try {
-        const response = await api.get('/hospital');
-        return response.data
-    } catch (error) {
-        throw error
-    }
 
+export async function listarHospitais() {
+  try {
+    const response = await api.get('/hospital');
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function cadastrarHospital(data: Omit<Hospital, 'id'>) {
+  try {
+    const response = await api.post('hospital', data)
+    return response
+  } catch (error) {
+    throw error
+  }
+}
+
+export async function buscarHospital(id: string) {
+  try {
+    const response = await api.get(`/hospital/${id}`)
+    return response
+  } catch (err) {
+    throw err
+  }
+}
+
+export async function atualizarHospital(id: string, data: Partial<Hospital>) {
+  try {
+    const response = await api.put(`/hospital/${id}`, data)
+    return response
+  } catch (err) {
+    throw err
+  }
+}
+
+export async function excluirHospital(id: string) {
+  try {
+    await api.delete(`/hospital/${id}`)
+
+  } catch (err) {
+    throw err
+  }
 }
