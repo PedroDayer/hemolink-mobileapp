c95781715d3d74c7b7c59cb28b62bc50f0cf11d5
Pedro Dayer <pedrob.dayer@gmail.com>
2026-06-21 19:23:42 -0300

    fix: correcao de bugs

--- src/pages/Catalogo/index.tsx
+++ src/pages/Catalogo/index.tsx
@@ -21,7 +21,7 @@ export const Catalogo = () => {
   const [hospitais, setHospitais] = useState<Hospital[]>([]);
   const [isLoading, setIsLoading] = useState<Boolean>(false);
   const [isDados, setIsDados] = useState<Boolean>(false);
-  const [filter, setFilter] = useState('')
+  const [filter, setFilter] = useState('');
 
   const [filtroSelecionado, setFiltroSelecionado] = useState<string>("1");
   const botoesFiltros = [
@@ -33,37 +33,21 @@ export const Catalogo = () => {
 
   async function carregarInformacoes() {
     setIsLoading(true);
-    // NOTA: FALTA VERIFICACAO DA CHEGADA DOS DADOS DA API + TOAST DE ERRO, CASO HAJA
     const response = await listarHospitais();
     if (response.status !== 200) {
-      // toast.error("Erro ao carregar as informações");
       setIsLoading(false);
       return;
     }
     setTimeout(() => {
       setHospitais(response.data);
-      // setResultadoFiltro(response.data);
       setIsLoading(false);
       setIsDados(true);
     }, 0);
-    console.log(response);
   }
+
   useEffect(() => {
     carregarInformacoes();
   }, []);
-
-  // useEffect(() => {
-  //   const getAllHospitais = async () => {
-  //     try {
-  //       const hospitais = await listarHospitais();
-  //       setHospitais(hospitais);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-
-  //   getAllHospitais();
-  // }, []);
 
   const hospitaisFiltrados = hospitais.filter((hospital) => {
     const textoFiltro = filter.toLowerCase().trim();
@@ -81,6 +65,51 @@ export const Catalogo = () => {
     return true;
   });
 
+  const renderHeader = () => (
+    <View>
+      <View style={styles.containerTitulo}>
+        <View style={styles.containerTituloFilho}>
+          <Text style={styles.title}>Sua jornada salva vidas</Text>
+          <Text style={styles.subTitle}>
+            Encontre o local ideal para sua próxima doação
+          </Text>
+        </View>
+        <View style={styles.containerInput}>
+          <Input
+            value={filter}
+            onChangeText={setFilter}
+            placeholder="Buscar hospitais ou cidades..."
+            iconLeft={<EvilIcons name="search" size={24} color="black" />}
+          />
+        </View>
+      </View>
+      
+      <FlatList
+        data={botoesFiltros}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingVertical: 10,
+        }}
+        renderItem={({ item }) => {
+          const ativo = item.id === filtroSelecionado;
+          return (
+            <View style={{ marginRight: 8 }}>
+              <Button
+                texto={item.nome}
+                onPress={() => setFiltroSelecionado(item.id)}
+                bg={ativo ? "#CE0C2C" : "#E2E8F0"}
+                color={ativo ? "#FFFFFF" : "#475569"}
+              />
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+
   return (
     <View style={styles.containerMain}>
       {isLoading ? (
@@ -90,79 +119,32 @@ export const Catalogo = () => {
       ) : isDados && hospitais.length === 0 ? (
         <EmptyState />
       ) : (
-        <>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <View style={styles.containerTitulo}>
-              <View style={styles.containerTituloFilho}>
-                <Text style={styles.title}>Sua jornada salva vidas</Text>
-                <Text style={styles.subTitle}>
-                  Encontre o local ideal para sua próxima doação
-                </Text>
-              </View>
-              <View style={styles.containerInput}>
-                <Input
-                  value={filter}
-                  onChangeText={setFilter}
-                  placeholder="Buscar hospitais ou cidades..."
-                  iconLeft={<EvilIcons name="search" size={24} color="black"   />}
-                  // falta colocar um height e um borderColor talvez
+        <FlatList
+          data={hospitaisFiltrados}
+          showsVerticalScrollIndicator={false}
+          ListHeaderComponent={renderHeader} 
+          contentContainerStyle={{
+            paddingBottom: 20,
+            gap: 12,
+          }}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => {
+            const { percentage } = obterBloodStock(item.bloodStock);
+            const tipoCritico = obterTiposSanguineosCriticos(item.bloodStock);
+            return (
+              <View style={styles.containerCard}>
+                <CardBaseCatalogo
+                  tipoCritico={tipoCritico}
+                  percentage={percentage}
+                  source={item.image}
+                  city={item.city}
+                  state={item.state}
+                  name={item.name}
                 />
               </View>
-            </View>
-            
-            <FlatList
-              data={botoesFiltros}
-              keyExtractor={(item) => item.id}
-              horizontal
-              showsHorizontalScrollIndicator={false}
-              contentContainerStyle={{
-                paddingHorizontal: 16,
-                paddingVertical: 10,
-              }}
-              renderItem={({ item }) => {
-                const ativo = item.id === filtroSelecionado;
-                return (
-                  <View style={{ marginRight: 8 }}>
-                    {" "}
-                    <Button
-                      texto={item.nome}
-                      onPress={() => setFiltroSelecionado(item.id)}
-                      bg={ativo ? "#CE0C2C" : "#E2E8F0"}
-                      color={ativo ? "#FFFFFF" : "#475569"}
-                    />
-                  </View>
-                );
-              }}
-            />
-
-            <View style={styles.containerCard}>
-              <FlatList
-                data={hospitaisFiltrados}
-                contentContainerStyle={{
-                  paddingBottom: 20,
-                  gap: 12,
-                }}
-                keyExtractor={(item) => String(item.id)}
-                renderItem={({ item }) => {
-                  const { percentage } = obterBloodStock(item.bloodStock);
-                  const tipoCritico = obterTiposSanguineosCriticos(
-                    item.bloodStock,
-                  );
-                  return (
-                    <CardBaseCatalogo
-                      tipoCritico={tipoCritico}
-                      percentage={percentage}
-                      source={item.image}
-                      city={item.city}
-                      state={item.state}
-                      name={item.name}
-                    />
-                  );
-                }}
-              />
-            </View>
-          </ScrollView>
-        </>
+            );
+          }}
+        />
       )}
     </View>
   );
